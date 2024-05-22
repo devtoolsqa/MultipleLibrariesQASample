cec40ff9736e3bc4a159bca647519f7eddbca5c4
sandeeppadala <spadala88@gmail.com>
2024-05-21 18:34:00 -0700

    Added navigation sample to project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,11 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation (libs.androidx.work.runtime.ktx)
+
 }
---
+++ app/src/main/java/com/example/kotlinpracticeudemy/appnavigations/NavigationActivity.kt
@@ -0,0 +1,25 @@
+package com.example.kotlinpracticeudemy.appnavigations
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import com.example.kotlinpracticeudemy.R
+
+class NavigationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_navigation)
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
